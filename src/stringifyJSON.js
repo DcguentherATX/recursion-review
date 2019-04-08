187014db8501a6bb28bd95a337e5fc36e2588e8d
DcguentherATX <davidcarlguenther@gmail.com>
2019-04-08 15:34:21 -0500

    completed JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -18,17 +18,56 @@ var stringifyJSON = function(obj) {
   if(obj === null){
     return 'null';
   }
+  // loop thru the array
+  // check each element with recursion 
+  //  '[1 , false, "hello" ]'
+  // result = result + ',' + 
+ //return '[' + result + ']' + "'"
+  
+  let results = [];
+
   if(Array.isArray(obj)){
-    let result = [];
-    // loop thru the array
-    // check each element with recursion 
-    //  '[1 , false, "hello" ]'
-    // result = result + ',' + 
-   return '[' + result + ']' + "'"
+    if(obj[0] === undefined) {
+      return '[]';
+    } else {
+      obj.forEach(element => {
+        results.push(stringifyJSON(element));
+      });
+      return "[" + results + "]";
+    }
+  }
+  if(typeof obj === 'object') {
+    for(var key in obj){
+      if(obj[key] !== undefined && typeof obj[key] !== 'function'){
+        var newKey = '"' + key + '"';
+        results.push(newKey + ':' + stringifyJSON(obj[key]))
+      }
+    }
+    return '{' + results + '}';
+
+
+    // var keys = Object.keys(obj);
+    // keys.forEach(key => {
+    //   var value = obj[key];
+    //   var prop = '"' + key + '":';
+
+    //   if(value === undefined) {
+    //     return '{}';
+    //   } else if (typeof value === 'function') {
+    //     results.push(''); 
+    //   } else if (typeof value === 'string') {
+    //     results.push(prop + '"' + value + '"');
+    //   } else if (typeof value === 'boolean' || typeof value === 'number' || typeof value === null) {
+    //     results.push(prop + value); 
+    //   } else if (typeof value === 'object') {
+    //     results.push(prop + stringifyJSON(value));
+    //   }
+    // });
+    // return '{' + results + '}';
+    //loop through obj
+    //if key is string, just make sure it stays a string
   }
 };
 
 
-
-// array
 // obj
